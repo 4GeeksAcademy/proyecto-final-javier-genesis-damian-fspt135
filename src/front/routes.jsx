088b813452851e64dcf9c6c3d3a3dfeb5261e1f9
JavierBrenner <javibrenner18@gmail.com>
2026-06-09 16:54:33 +0000

    FRONT register form and dataprofile

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -9,6 +9,8 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
+import { RegisterForm } from "./pages/RegisterForm"
+import { DataProfile } from "./pages/DataProfile"
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -25,6 +27,8 @@ export const router = createBrowserRouter(
         <Route path= "/" element={<Home />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/data-profile" element={<DataProfile />} />
       </Route>
     )
 );
