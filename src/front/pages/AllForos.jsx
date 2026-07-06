5a55688d4aa1960cf5409acd39148e6c2a16bcc8
JavierBrenner <javibrenner18@gmail.com>
2026-07-06 17:53:54 +0000

    fix bug in allforos

--- src/front/pages/AllForos.jsx
+++ src/front/pages/AllForos.jsx
@@ -1,31 +1,42 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CardForoSimply } from "../components/CardForo.jsx";
 import { useForo } from "../hooks/Hooks_foro/useForo.js";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import { useState } from "react";
 import { EditForoModal } from "../components/EditForoModal.jsx";
+
 
 export const AllForos = () => {
     const { store, dispatch } = useGlobalReducer()
+    const [ foros, setForos ] = useState([])
 
     const { getDataForo } = useForo();
 
+    const getDataForos = async() => {
+        try { 
+            const data = await getDataForo();
+            setForos(data)
+
+        } catch(err) {
+
+        }
+    
+    } 
     useEffect(() => {
         console.log("estoy en los foros");
-
-        getDataForo();
+        getDataForos()
     }, []);
 
     const [editingForo, setEditingForo] = useState(null);
     const user = JSON.parse(localStorage.getItem("user"));
-
+    console.log(foros);
+    
     return (
         <div className="container py-4">
 
             <div className="d-flex flex-wrap gap-2 border p-3 rounded bg-light overflow-y-auto">
-                {store.foros && store.foros.map((foro) => {
+                {foros.map((foro) => {
                     return (
                         <div className="card shadow-sm border rounded-3 overflow-hidden bg-white">
                             <div>
